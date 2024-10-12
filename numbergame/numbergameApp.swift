948d90f0274a023c6ef739285b105e99daa49f71
Danil Chernov <realdnchka@proton.me>
2024-10-12 22:10:56 +0700

    Initial Commit

---
+++ numbergame/numbergameApp.swift
@@ -0,0 +1,17 @@
+//
+//  numbergameApp.swift
+//  numbergame
+//
+//  Created by Danil Chernov on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct numbergameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ numbergameTests/numbergameTests.swift
@@ -0,0 +1,16 @@
+//
+//  numbergameTests.swift
+//  numbergameTests
+//
+//  Created by Danil Chernov on 12/10/24.
+//
+
+import Testing
+
+struct numbergameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
