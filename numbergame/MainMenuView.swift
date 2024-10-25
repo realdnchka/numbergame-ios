889e1d5077d50c226f77e240f71de24e30d0994c
Danil Chernov <realdnchka@proton.me>
2024-10-25 07:55:53 +0700

    Add loader for MainActivity

--- numbergame/MainMenuView.swift
+++ numbergame/MainMenuView.swift
@@ -11,21 +11,47 @@ struct MainMenu: View {
     private let menuItems: [String] = ["New Game", "Leaderboard", "Profile", "Settings"]
     @State var numbers: Numbers?
     @State var isShowingMenu: Bool = false
-    @State var isMenuDisabled = true
     @State var isButtonPressed: Bool = false
-    
+    @State private var rotationAngle = -0.1
+    @State private var size = 128.0
+    @State private var blur = 1.0
     var body: some View {
         NavigationStack {
             ZStack {
                 Color("Background")
                     .ignoresSafeArea(.all)
-                VStack {
-                    Image(.mainMenuLogo)
-                    MenuButton2(text: menuItems[0], isDisabled: isMenuDisabled, destination: GameView(numbers: numbers, sum: numbers?.sum))
-                    
-                    MenuButton(text: menuItems[1], isDisabled: true,action: {})
-                    MenuButton(text: menuItems[2], isDisabled: true,action: {})
-                    MenuButton(text: menuItems[3], isDisabled: true,action: {})
+                if (numbers == nil && rotationAngle != 0) {
+                    Image(.loaderIcon)
+                        .resizable()
+                        .blur(radius: 1)
+                        .frame(width: size, height: size)
+                        .rotationEffect(.degrees(rotationAngle))
+                        .onAppear {
+                            withAnimation(.linear(duration: 2)
+                                .repeatForever(autoreverses: false)) {
+                                    rotationAngle = 360.0
+                                }
+                        }
+                }
+                else {
+                    VStack {
+                        Image(.loaderIcon)
+                            .resizable()
+                            .frame(width: size, height: size)
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 2)
+                                    .repeatForever(autoreverses: true)) {
+                                        rotationAngle = 360.0
+                                        size += 16
+                                    }
+                            }
+                        Divider()
+                        MenuButton2(text: menuItems[0], destination: GameView(numbers: numbers, sum: numbers?.sum))
+                        
+                        MenuButton(text: menuItems[1], isDisabled: true,action: {})
+                        MenuButton(text: menuItems[2], isDisabled: true,action: {})
+                        MenuButton(text: menuItems[3], isDisabled: true,action: {})
+                    }
                 }
             }
             .navigationBarHidden(true)
@@ -33,7 +59,6 @@ struct MainMenu: View {
         .onAppear() {
             Task {
                 numbers = await getNumbers()
-                isMenuDisabled.toggle()
             }
         }
     }
