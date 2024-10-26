18c10e518ad32e5d0dcffcff72cea764076d6951
Danil Chernov <realdnchka@proton.me>
2024-10-26 13:48:23 +0700

    small changes

--- numbergame/Elements/MenuButton.swift
+++ numbergame/Elements/MenuButton.swift
@@ -5,36 +5,7 @@
 //  Created by Danil Chernov on 23/10/24.
 //
 
-import SwiftUICore
 import SwiftUI
-
-//struct MenuButton: View {
-//    var text: String
-//    var isDisabled: Bool = false
-//    var isPressed: Bool = false
-//    var destination: AnyView
-//    @State var showDestination: Bool = false
-//    
-//    var body: some View {
-//        
-//        Button(text) {
-//            withAnimation(.easeInOut) {
-//                showDestination.toggle()
-//            }
-//        }
-//        .buttonStyle(isPressed || isDisabled ? AnyButtonStyle(MenuButtonPressedStyle()) : AnyButtonStyle(MenuButtonStyle()))
-//        .disabled(isDisabled)
-//        .animation(.easeInOut(duration: 0.2), value: isPressed)
-//        .frame(width: 116, height: 32)
-//        .cornerRadius(15.0)
-//        .shadow(radius: 5.0)
-//        
-//        if showDestination {
-//            destination
-//                .transition(.opacity)
-//        }
-//    }
-//}
 
 struct MenuButton<Destination: View>: View {
     var text: String
